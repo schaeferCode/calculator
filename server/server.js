216bb05fca5ad8e1f0083ccd0f2d0189418a255b
Scott Schaefer <scottschaef@gmail.com>
2017-12-27 10:32:33 -0500

    Create server and test html

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const open = require('open');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const server = app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:${port}');
+    console.log(`Listening on port ${port}`);
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'dist')));
